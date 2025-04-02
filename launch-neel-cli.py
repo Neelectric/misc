6abcf2b030178e795fad5fab64420ea76522f140
Neel Rajani <97137109+Neelectric@users.noreply.github.com>
2025-04-03 00:16:09 +0100

    Update launch-neel-cli.py

--- launch-neel-cli.py
+++ launch-neel-cli.py
@@ -41,6 +41,7 @@ def main():
         "wget -qO- https://astral.sh/uv/install.sh | sh && " \
         "source $HOME/.local/bin/env && " \
         "cd open-r1_olmo && " \
+        "bash setup.bash && " \
         "git lfs install && " \
         "pip install -U pip && " \
         "pip install datasets && " \
